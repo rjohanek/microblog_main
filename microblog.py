82210bd2785ec5cac589c391b0c818ac5a3de65d
Rachel Johanek <johanek.r@husky.neu.edu>
2023-04-17 01:56:05 +0000

    add signup model to microblog.py

--- microblog.py
+++ microblog.py
@@ -1,5 +1,5 @@
 from app import create_app, db, cli
-from app.models import User, Post, Message, Notification, Task
+from app.models import User, Post, Message, Notification, Task, SignUp
 
 app = create_app()
 cli.register(app)
@@ -8,4 +8,4 @@ cli.register(app)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
-            'Notification': Notification, 'Task': Task}
+            'Notification': Notification, 'Task': Task, 'SignUp': SignUp}
